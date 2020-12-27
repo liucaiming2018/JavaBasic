0c10b1b6b06c83b57470ae6dd9df2988ded01c12
liujunguo2012@126.com <liujunguo2012@126.com>
2020-12-27 15:31:50 +0800

    JVM

---
+++ JVM/src/main/java/com/lcm/jvm/gc/HelloGC.java
@@ -0,0 +1,16 @@
+package com.lcm.jvm.gc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HelloGC {
+
+    public static void main(String[] args) {
+        System.out.println("HelloGC!");
+        List list = new LinkedList();
+        for(;;) {
+            byte[] b = new byte[1024*1024];
+            list.add(b);
+        }
+    }
+}
